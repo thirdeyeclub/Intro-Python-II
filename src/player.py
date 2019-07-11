160a75d4306c471dc3e6639d867ba211705c51d1
Taylor <wwwaaavvveeerrr@gmail.com>
2019-07-11 11:34:56 -0400

    fixed imports

--- src/player.py
+++ src/player.py
@@ -1,28 +1,25 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-
+from item import Item
 
 class Player:
-    def __init__(self, current_room):
-        self.current_room = current_room
+    def __init__(self, _current_room):
+        self._current_room = _current_room
         self.items = []
         self.ego = 4
 
     def move_rooms(self, direction):
-        if self.current_room.move_rooms(direction) != 'error':
-            self.current_room = self.current_room.move_rooms(direction)
-            return f'Moved to room {self.current_room.name}'
+        if self._current_room.move_rooms(direction) != 'error':
+            self._current_room = self._current_room.move_rooms(direction)
+            return f'Moved to room {self._current_room.name}'
         else:
             return 'There is no room that way'
 
-    def get_item(self, item):
-        self.items.append(item)
+    def get_item(self, Item):
+        self.items.append(Item)
 
-    def drop_item(self, item):
-        self.items.remove(item)
-
-	def drop_item(self, item):
-		if item in self.items:
-			self.items.remove(item)
+	def drop_item(self, Item):
+		if Item in self.items:
+			self.items.remove.Item)
 		else:
 			print(f"You are not carrying an item")
